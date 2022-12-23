bb69687a5511e520d4f61aabdd3ec86ffad1b07a
Gregory Starr <gregory.a.starr@gmail.com>
2022-12-22 19:21:11 -0500

    adding app code

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,94 @@
-import { useState } from 'react'
-import logo from './logo.svg';
-import './App.css';
-import { MultiUpload } from './components/multi-upload';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { MultiUpload } from "./components/multi-upload";
+import { MultiUploadService } from "./services/multi-upload";
+
+const App = () => {
+  // Flags
+  const [UPLOAD_COMPLETE, setUploadComplete] = useState(false);
+  const [IS_UPLOADING, setIsUploading] = useState(false);
+  const [HAS_ERROR, setHasError] = useState(false);
 
 
+  useEffect(()=>{},[])
 
-function App() {
-    const [filesSelected, setFilesSelected] = useState(false)
+  // Handlers
+  function onFilesHandler(batchName, files) {
+    if (!files.length) {
+      const error = new Error("Please select files");
 
-
-    // Handlers
-    onFilesHandler = (files) => {
-
+      setHasError(error.message);
+      //throw(error)
     }
 
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        App Header <
-        /header>
+    if (!batchName) {
+      batchName = `${files[0].name.split(".")[0]}-Project`;
+    }
 
-        <
-        div >
-        <
-        section >
-        content area
+    const tempArray = [];
+    for (const file of files) {
+      tempArray.push(file);
+    }
 
-        upload files <
-        MultiUpload onFiles = {}
-        /> <
-        /section> <
-        /div> <
-        /div>
-    );
-}
+    // now call upload service
+    uploadService(batchName, tempArray);
+    setIsUploading(true);
+  }
+
+
+  const onBatchUploadSuccess = (data) => {
+    setIsUploading(false);
+    setUploadComplete(true);
+    setHasError(false)
+    console.log(`from batch success handler: ${JSON.stringify(data)}`);
+  };
+
+  const onBatchUploadError = (err) => {
+    setHasError(err.message);
+    console.log(`from batch error handler: ${err}`);
+  };
+
+  // instantiate the service
+  const uploadService = (name, files) => {
+    if (!files || !files.length) {
+      const error = new Error("files have not been set properly");
+      setHasError(error);
+    }
+
+    const uploadPath = name;
+    setIsUploading(true);
+    try {
+       MultiUploadService(
+        uploadPath,
+        files,
+        onBatchUploadSuccess,
+        onBatchUploadError
+      );
+    } catch (error) {
+      setHasError(error);
+      setIsUploading(false);
+      setUploadComplete(false)
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        Convert your photos to 3D  <hr />
+      </header>
+
+      <div>
+        <section className="upload-container">
+          UPLOAD YOUR FILES
+          <MultiUpload onFiles={onFilesHandler} />
+          {!UPLOAD_COMPLETE && !HAS_ERROR && IS_UPLOADING &&
+            `{$Files.length} files have been selected, now uploading`}
+          {HAS_ERROR && <div className="error">{HAS_ERROR}</div>}
+          {UPLOAD_COMPLETE && <div className="success">Upload Complete</div>}
+        </section>
+      </div>
+    </div>
+  );
+};
 
 export default App;
