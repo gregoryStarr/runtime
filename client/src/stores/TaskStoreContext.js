e0367cd953e46647c8ffc9f00b5c9d35f93cf484
Gregory Starr <gregory.a.starr@gmail.com>
2022-12-28 03:53:11 -0500

    define mobx store, stoere context, and task data structure rto be p-assed around

---
+++ client/src/stores/TaskStoreContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+import { TaskStore } from "../stores/TaskStore";
+
+const TaskStoreContext = createContext(new TaskStore());
+
+export default TaskStoreContext;
